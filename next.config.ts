611aba9df9334089ee72516af867a5493714867c
gabriellamsa <gabii.marcelly@gmail.com>
2026-05-08 18:10:07 +0700

    feat: establish visual app shell

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    localPatterns: [
+      {
+        pathname: "/images/**",
+        search: "",
+      },
+      {
+        pathname: "/images/asia-facade.png",
+        search: "?v=20260507-160908",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
